fd42d3a41e4e484e210cb8eab1c901c0f0feb7fd
skkaz7 <s.k.kazmierczak7@gmail.com>
2022-12-23 12:44:38 +0100

    10_Dzien_16-17/01_Formularze/Zad 6 + 01_useEffect_-_ciag_dalszy_zadan/zad 1

--- 08_Dzien_12-14/01_useEffect_-_ciag_dalszy_zadan/01_Zadanie_1/js/app.js
+++ 08_Dzien_12-14/01_useEffect_-_ciag_dalszy_zadan/01_Zadanie_1/js/app.js
@@ -1,7 +1,26 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <h1>Hello, World!</h1>,
-  document.getElementById("app")
-);
+function PropsToState(props) {
+    const [info, setInfo] = useState(props.text)
+
+    useEffect(() => {
+        const myInterval = setInterval(() => {
+            setInfo(prevState => prevState + '.')
+        }, 1000)
+
+        return () => clearInterval(myInterval)
+    }, [])
+
+    return <ShowInfo info={info}/>
+}
+
+function ShowInfo(props) {
+    return <h1>{props.info}</h1>
+}
+
+function App() {
+    return <PropsToState text=''/>
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));
